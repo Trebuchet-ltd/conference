f4bfd9e345d6015ba3f2cdaacbb9ff4a23ac8c95
Jyothis P <jyothisp52@gmail.com>
2020-08-29 19:58:32 +0530

    Tidy up connections

--- PaperSub/models.py
+++ PaperSub/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Paper(models.Model):
-    id=models.AutoField(primary_key=True)
-    title=models.CharField(max_length=255)
-    description=models.CharField(max_length=1000)
-    status=models.CharField(max_length=255)
-    comment=models.CharField(max_length=255)
-    keyword=models.CharField(max_length=255)
-    file=models.FileField(upload_to='paper/')
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    status = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255)
+    keyword = models.CharField(max_length=255)
+    file = models.FileField(upload_to='papers/')
--- conference/settings.py
+++ conference/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ConferenceDash',
+    'Organiser',
+    'PaperSub',
+    'Payment',
+    'PosterSub',
+    'Profile',
+    'Reviewer',
+    'Workshop',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
