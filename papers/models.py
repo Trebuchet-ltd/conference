95751b4b6be5c289a9732d59f95426604b8b5b49
Jyothis P <jyothisp52@gmail.com>
2020-09-02 19:23:17 +0530

    add paper-user reln

--- papers/models.py
+++ papers/models.py
@@ -19,7 +19,9 @@ class Paper(models.Model):
     keyword = models.CharField(max_length=255)
     file = models.FileField(upload_to=media_location, null=True)
     is_poster = models.BooleanField(default=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='papers')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='submissions')
+    approved_paper = models.OneToOneField(User, on_delete=models.CASCADE, related_name='paper', null=True)
+    approved_poster = models.OneToOneField(User, on_delete=models.CASCADE, related_name='poster', null=True)
 
     def __str__(self):
         return self.title
