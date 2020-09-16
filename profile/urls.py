015d2c7315097c2e0c1a09e3058b4f28f3421112
Jyothis P <jyothisp52@gmail.com>
2020-09-16 17:02:23 +0530

    Update urls.py

--- profile/urls.py
+++ profile/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = 'profile'
 
 urlpatterns = [
-    url('users', views.UserList.as_view()),
+    # url('users', views.UserList.as_view()),
     url('registration/complete', views.complete_view, name='account_confirm_complete'),
 ]
