9d26383ad47aa60b9710d2fb527cf46383b0e75f
Deathblade6 <arun.padmanabhan00@gmail.com>
2020-09-17 00:44:56 +0530

    Production changes

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,9 @@ if 'DJANGO_SETTINGS' in os.environ:
     if os.environ['DJANGO_SETTINGS'] == "dev":
         print("DEVELOPMENT SERVER")
         from .development_settings import *
+    else:
+        print("PRODUCTION SERVER")
+        from .production_settings import *
 else:
     print("PRODUCTION SERVER")
     from .production_settings import *
