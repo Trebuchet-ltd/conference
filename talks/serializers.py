18da63b84edefe9d6b778509ee5bab2944117b51
Jyothis P <jyothisp52@gmail.com>
2020-09-24 17:24:52 +0530

    Populated Sessions.

--- talks/serializers.py
+++ talks/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
 from .models import *
+from profile.models import User
+
+
+class SmallUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'payment_status', 'nationality', 'designation', 'affiliation',
+                  'highest_degree', 'subject', 'specialization']
 
 
 class ProgramSerializer(serializers.ModelSerializer):
@@ -16,6 +24,7 @@ class BaseParticipantSerializer(serializers.ModelSerializer):
 
 class SessionSerializer(serializers.ModelSerializer):
     participants = BaseParticipantSerializer(many=True)
+    organiser = SmallUserSerializer()
 
     class Meta:
         model = Session
