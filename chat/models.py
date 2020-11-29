00a0b02b28ad29f9fff5ad96156b8b6c860d62d0
Deathblade6 <arun.padmanabhan00@gmail.com>
2020-11-30 05:07:22 +0530

    Fixing dumb chat stuff

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Chat(models.Model):
+    class Meta:
+        app_label = 'chat'
+
     timestamp = models.IntegerField(null=False,blank=False)
     uid = models.CharField(max_length=255)
     user_name = models.CharField(max_length=255)
