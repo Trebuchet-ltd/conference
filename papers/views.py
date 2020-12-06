a013670cb9c5eaafa71cefaea141ee02e24d691c
Jyothis P <jyothisp52@gmail.com>
2020-12-06 12:47:41 +0530

    Paper upload author required fix.

--- papers/views.py
+++ papers/views.py
@@ -116,7 +116,7 @@ class PaperViewset(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_serializer_class(self):
-        print('Paper get serializer:', self.request.user.role, self.action)
+        print('Paper get_serializer_class:', self.request.user.role, self.action)
         if self.request.user.role == 'organiser':
             return OrganiserPaperSerializer
         elif self.request.user.role == 'reviewer':
