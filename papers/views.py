a84bc28bf099bf3696bb17a04dc89a42bf6c9df8
Jyothis P <jyothisp52@gmail.com>
2020-12-08 19:08:38 +0530

    Fixing plenary - debug

--- papers/views.py
+++ papers/views.py
@@ -129,18 +129,18 @@ class PaperViewset(viewsets.ModelViewSet):
         return PaperSerializer
 
 
+class PublicPlenarySpeakersList(ListAPIView):
+    queryset = Paper.objects.filter(author__is_plenary__exact=True)
+    print('Queryset length [AnonPlenaryList]:', len(queryset))
+    serializer_class = SmallPaperSerializer
+
+
 class AnonPaperList(ListAPIView):
     queryset = Paper.objects.all()
     print('Queryset length [AnonPaperList]:', len(queryset))
     serializer_class = SmallPaperSerializer
     permission_classes = [permissions.AllowAny]
     http_method_names = ["get"]
-
-
-class PublicPlenarySpeakersList(ListAPIView):
-    queryset = Paper.objects.filter(author__is_plenary__exact=True)
-    print('Queryset length [AnonPlenaryList]:', len(queryset))
-    serializer_class = SmallPaperSerializer
 
 
 class PaperList(ListAPIView):
