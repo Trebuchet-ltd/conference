28debb519dfaea4b8ad6269141bc4cdce3bec52b
Jyothis P <jyothisp52@gmail.com>
2020-12-24 15:52:48 +0530

    debug

--- scripts/convert_resolution.py
+++ scripts/convert_resolution.py
@@ -5,26 +5,28 @@ PROGRESS_LOADER = ['-', '\\', '|', '/']
 EMOTES = ['└|∵|┐ ( ͡°ᴥ ͡° ʋ)', '┌|∵|┘ (ʋ  ͡°ᴥ ͡°)']
 
 VIDEO_OUTPUT_CONFIG = {
-    # 'low': {
-    #     'codec': 'h264',
-    #     'width': 256,
-    #     'height': 144,
-    #     'fps': 60
-    # },
+    'low': {
+        'codec': 'h264',
+        'width': 256,
+        'height': 144,
+        'fps': 60
+    },
     'mid': {
         'codec': 'h264',
         'width': 640,
         'height': 360,
         'fps': 60
     },
-    # 'high': {
-    #     'codec': 'h264',
-    #     'width': 1280,
-    #     'height': 720,
-    #     'fps': 60
-    # },
+    'high': {
+        'codec': 'h264',
+        'width': 1280,
+        'height': 720,
+        'fps': 60
+    },
 
 }
+
+
 
 OUTPUT_CONFIG = {
     'format': 'mp4',
@@ -36,28 +38,42 @@ OUTPUT_CONFIG = {
 }
 
 
-def get_resolutions(file_name):
-    conv = Converter()
-    print('Checking input video aspect ratio.')
+def get_info(conv, file_name):
     info = conv.probe(file_name)
-    print(info)
-    # asp_ratio = info.video.video_width / info.video.video_height
-    # if asp_ratio != (16 / 9):
-    #     print(f'Input resolution of {info.video.video_width}x{info.video.video_height} is not 16:9. Skipping.')
-    #     return
+    if video in info:
+        print('1')
+        print(info.video.video_width, info.video.video_height)
+        return info.video.video_width / info.video.video_height
+    else:
+        for stream in info.streams:
+            if width in stream:
+                print('2')
+                print(stream.width, stream.height)
+                return stream.width / stream.height
 
-    for res in VIDEO_OUTPUT_CONFIG:
-        i = 0
-        output_file_name = '.'.join(file_name.split('.')[:-1]) + '_' + res + '.mp4'
-        OUTPUT_CONFIG['video'] = VIDEO_OUTPUT_CONFIG[res]
-        convert = conv.convert(file_name, output_file_name, OUTPUT_CONFIG)
-        print('Converting to', res, 'res.')
-        for timecode in convert:
-            print(f'\rConverting {timecode * 100:.1f}% * {PROGRESS_LOADER[i % 4]} * {EMOTES[i % 2]}  ', end='',
-                  flush=True)
-            i += 1
-        print('\rConversion completed.                                   ')
-        print('Writing to file:', output_file_name)
+
+# def get_resolutions(file_name):
+#     conv = Converter()
+#     print('Checking input video aspect ratio.')
+#     info = conv.probe(file_name)
+#     print(info)
+#     # asp_ratio = info.video.video_width / info.video.video_height
+#     # if asp_ratio != (16 / 9):
+#     #     print(f'Input resolution of {info.video.video_width}x{info.video.video_height} is not 16:9. Skipping.')
+#     #     return
+#
+#     for res in VIDEO_OUTPUT_CONFIG:
+#         i = 0
+#         output_file_name = '.'.join(file_name.split('.')[:-1]) + '_' + res + '.mp4'
+#         OUTPUT_CONFIG['video'] = VIDEO_OUTPUT_CONFIG[res]
+#         convert = conv.convert(file_name, output_file_name, OUTPUT_CONFIG)
+#         print('Converting to', res, 'res.')
+#         for timecode in convert:
+#             print(f'\rConverting {timecode * 100:.1f}% * {PROGRESS_LOADER[i % 4]} * {EMOTES[i % 2]}  ', end='',
+#                   flush=True)
+#             i += 1
+#         print('\rConversion completed.                                   ')
+#         print('Writing to file:', output_file_name)
 
 
 if __name__ == '__main__':
@@ -72,4 +88,7 @@ if __name__ == '__main__':
     if ext.lower() not in ['mkv', 'mp4']:
         print(file, 'does not seem to be a video. Skipping.')
     else:
-        get_resolutions(file)
+        conv = Converter()
+        asp = get_info(conv, file)
+        print('Aspect:', asp)
+        # get_resolutions(file)
