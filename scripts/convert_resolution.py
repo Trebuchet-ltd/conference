25cdfb91f46c6b49871ae4cc7f67a896585ad50a
Jyothis P <jyothisp52@gmail.com>
2020-12-26 14:49:42 +0530

    add extensions

--- scripts/convert_resolution.py
+++ scripts/convert_resolution.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     logging.info(f'Current file: {file}')
 
     ext = file.split('.')[-1]
-    if ext.lower() not in ['mkv', 'mp4']:
+    if ext.lower() not in ['mkv', 'mp4', 'mov']:
         logging.info(f'{file} does not seem to be a video. Skipping.')
     else:
         get_resolutions(file)
